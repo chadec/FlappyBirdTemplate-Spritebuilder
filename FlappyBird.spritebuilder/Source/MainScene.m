0e5e09bac96778a347eb4f9a425fb339e37339f7
chadec <chad@dubbysattractions.com>
2015-06-13 17:05:07 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -13,6 +13,12 @@
     CCNode *_ground1;
     CCNode *_ground2;
     NSArray *_grounds;
+    CCNode *_cloud1;
+    CCNode *_cloud2;
+    NSArray *_clouds;
+    CCNode *_bush1;
+    CCNode *_bush2;
+    NSArray *_bushes;
     
     NSTimeInterval _sinceTouch;
     
@@ -32,6 +38,8 @@
     self.userInteractionEnabled = TRUE;
     
     _grounds = @[_ground1, _ground2];
+    _clouds  = @[_cloud1, _cloud2];
+    _bushes  = @[_bush1, _bush2];
     
     for (CCNode *ground in _grounds) {
         // set collision txpe
@@ -143,6 +151,30 @@
             ground.position = ccp(ground.position.x + 2 * ground.contentSize.width, ground.position.y);
         }
     }
+    
+    // move and loop the bushes
+    for (CCNode *bush in _bushes) {
+        // move the bush
+        bush.position = ccp(bush.position.x - (character.physicsBody.velocity.x * delta), bush.position.y);
+        // if left corner is completely off screen, move it right
+        if (bush.position.x <= (-1 * bush.contentSize.width)) {
+            bush.position = ccp(bush.position.x + 2 * bush.contentSize.width, bush.position.y);
+        }
+    }
+    
+    // move and loop the clouds
+    for (CCNode *cloud in _clouds) {
+        // move the clouds
+        cloud.position = ccp(cloud.position.x -
+                             (character.physicsBody.velocity.x * delta), cloud.position.y);
+        //loop
+        if (cloud.position.x <= (-1 * cloud.contentSize.width)) {
+            cloud.position = ccp(cloud.position.x + 2 * cloud.contentSize.width, cloud.position.y);
+        }
+        
+    }
+    
+    
     
     NSMutableArray *offScreenObstacles = nil;
     
